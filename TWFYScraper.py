51bed0fcc3e0503ae96e60d3b0cadb4ab243cc7f
James Cornall <jjc257@student.bham.ac.uk>
2023-08-16 18:52:36 +0100

    Add if clause to check whether target file exists

--- TWFYScraper.py
+++ TWFYScraper.py
@@ -9,12 +9,14 @@ from bs4 import BeautifulSoup as bs
 _URL = "https://www.theyworkforyou.com/pwdata/scrapedxml/debates/"
 page = requests.get(_URL)
 
-# Function to check whether .xml file corresponds to debates after 2012
-year = 2012
+# Define scraper parameters
+_YEAR = 2023
+_OUTPUT_FOLDER = "E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\xml"
 
+# Function to check whether .xml file corresponds to debates after given date
 def isAfter(string):
     number = re.findall(r'\d+', string)
-    if int(number[0]) > year:
+    if int(number[0]) > _YEAR:
         return True
     else:
         return False
@@ -22,22 +24,12 @@ def isAfter(string):
 # Instantiate BeautifulSoup object
 soup = bs(page.content, "html.parser")
 
-# Loops through index table and adds relevant rows to new list
-# table = soup.find("table")
-# rows=list()
-# for row in table.find_all("a"):
-#     if (row.text).startswith("debates"):
-#         if isAfter2012:
-#             print(row.text)
-#             rows.append(row)
-
 urls = []
 names = []
 
 # Loops through all classes tagged "a" and adds relevant names/URLs to list
 for i, link in enumerate(soup.findAll("a")):
     _FULLURL = _URL + link.get("href")
-    print(_FULLURL)
     if _FULLURL.endswith(".xml"):
         if isAfter(_FULLURL):
             urls.append(_FULLURL)
@@ -45,28 +37,24 @@ for i, link in enumerate(soup.findAll("a")):
 
 # Combines the names/URLs lists 
 names_urls = zip(names, urls)
-print(names_urls)
 
-# Downloads the files using the URLs in the zipped list
-# def file_exists(path):
-#     return os.path.exists(path)
-
+# Function to download .xml files 
 def scrape_file(name, url):
-    # if file_exists(file_path):
-    #     print(f"File '{name}' already exists. Skipping...")
-    #     return
-    print("Downloading %s" % url)
-    r = requests.get(url)
-    with open("E:\Documents\Education\PG University of Birmingham\MSc Computer Science\Summer Semester\MSc Projects\Project Files\Dataset\xml" + name.split("/")[-1], "wb") as f:
-        f.write(r.content)
-
-for name, url in names_urls:
+    print(_OUTPUT_FOLDER + "\\" + name)
+    if os.path.exists(_OUTPUT_FOLDER + "\\" + name) == True:
+        print(f"File '{name}' already exists. Skipping...")
+    else:
+        print("Downloading %s" % url)
+        r = requests.get(url)
+        with open(_OUTPUT_FOLDER + name.split("/")[-1], "wb") as f:
+            f.write(r.content)
+            
+for name, url in zip(names, urls):
+    print(f"{name}" + f"{url}")
+            
+# Loops through and downloads the files using the URLs in the zipped list
+print("Initiating scraping loop")
+for name, url in list(names_urls):
+    print(f"Scraping '{name}")
     scrape_file(name, url)
-
-# for name, url in names_urls:
-#     print(url)
-#     rq = urllib3.request(url)
-#     res = urllib3.urlopen(rq)
-#     xml = open("xmls/" + name, 'wb')
-#     xml.write(res.read())
-#     xml.close()    
+    
