75e2681d5ffae9de2d7f8b57015d3562391b0777
James Cornall <jjc257@student.bham.ac.uk>
2023-08-17 14:10:27 +0100

    Modify isAfter function

--- TWFYScraper.py
+++ TWFYScraper.py
@@ -1,7 +1,5 @@
 import re
 import requests
-import pandas as pd
-import urllib3
 import os
 from bs4 import BeautifulSoup as bs
 
@@ -11,12 +9,15 @@ page = requests.get(_URL)
 
 # Define scraper parameters
 _YEAR = 2023
-_OUTPUT_FOLDER = "E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\xml"
+_OUTPUT_FOLDER = "E:\\Documents\\Education\\PG University of Birmingham\\MSc Computer Science\\Summer Semester\\MSc Projects\\Project Files\\Dataset\\xml\\"
+
+print("Scraping TWFY .xml debate transcripts from " + f"{_YEAR} onwards...")
+print("Outputting to " + f"'{_OUTPUT_FOLDER}'..." + "\n")
 
 # Function to check whether .xml file corresponds to debates after given date
 def isAfter(string):
     number = re.findall(r'\d+', string)
-    if int(number[0]) > _YEAR:
+    if int(number[0]) >= _YEAR:
         return True
     else:
         return False
@@ -37,24 +38,28 @@ for i, link in enumerate(soup.findAll("a")):
 
 # Combines the names/URLs lists 
 names_urls = zip(names, urls)
+print(names_urls)
 
 # Function to download .xml files 
 def scrape_file(name, url):
     print(_OUTPUT_FOLDER + "\\" + name)
     if os.path.exists(_OUTPUT_FOLDER + "\\" + name) == True:
-        print(f"File '{name}' already exists. Skipping...")
+        print(f"File '{name}' already exists. Skipping..." + "\n")
     else:
-        print("Downloading %s" % url)
+        print("Downloading %s" % url + ".\n")
         r = requests.get(url)
         with open(_OUTPUT_FOLDER + name.split("/")[-1], "wb") as f:
             f.write(r.content)
-            
+        
 for name, url in zip(names, urls):
     print(f"{name}" + f"{url}")
             
 # Loops through and downloads the files using the URLs in the zipped list
 print("Initiating scraping loop")
 for name, url in list(names_urls):
-    print(f"Scraping '{name}")
+    print(f"Scraping '{name}'.")
     scrape_file(name, url)
     
+print("Scraping complete...\n")
+    #   + f"{skipped}" + " files skipped...\n"
+    #   + f"{downloaded}" + " files downloaded...\n")
